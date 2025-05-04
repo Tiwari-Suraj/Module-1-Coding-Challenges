bd12e4fa3f688374738779d8985138196cb0dc3c
Suraj tiwari <tiwarisuraj76090@gmail.com>
2025-05-04 21:12:34 +0530

    solve Question 5

--- main.js
+++ main.js
@@ -211,6 +211,27 @@
 
 // code
 
+
+function countVowels(str) {
+    const vowels = 'aeiou';
+    let count = 0;
+
+    for (let char of str.toLowerCase()) {
+        if (vowels.includes(char)) {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+// Example test cases
+console.log(countVowels("Hello World"));
+console.log(countVowels("AEIOU"));
+console.log(countVowels("xyz"));
+
+
+
 //
 
 //    ####   Improvised Perspective to the Coding Problem
